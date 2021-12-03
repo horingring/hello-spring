b8163d59526653be75659a7ef7ec317e0d430800
Horingring <dldldksl@naver.com>
2021-12-03 19:37:21 +0900

    fix(스프링 빈과 의존관계): 자바 코드로 직접 스프링 빈 등록하기
    [ 이론 ]
    : 기존 @Service, @Repository 어노테이션을 사용하지 않고 설정파일(SpringConfig.java)을
    만들어 자바 코드로 직접 스프링 빈을 (Spring Container에) 등록하는 방법
      ( * 예전엔 xml로 작성했으나, 현재는 이 방법은 거의 사용하지 않음 )
    : 회원 서비스와 회원 리포지토리의 @Service, @Repository, @Autowired 어노테이션을 제거하고 진행

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
 
@@ -18,7 +17,6 @@ public class MemberService {
     //  인스턴스 생성 시 외부에서 MemberRepository를 입력
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {  // 생성자
         this.memberRepository = memberRepository;
     }
